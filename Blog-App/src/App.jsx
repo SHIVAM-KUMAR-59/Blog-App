62d55019acd01fd5159861cd5e20065cecd736df
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-06 20:15:02 +0530

    Initialized Project

--- Blog-App/src/App.jsx
+++ Blog-App/src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-    <h1 className = "text-3xl fint-bold underline">Blog App</h1>
+      <h1 className="text-3xl fint-bold underline">Blog App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
