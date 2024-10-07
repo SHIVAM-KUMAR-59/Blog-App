8286337f94ad4d62c72e3f1b50ff8f440ce2e51f
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-07 12:45:43 +0530

    Get Posts By Id Function Added

--- Backend/src/index.mjs
+++ Backend/src/index.mjs
@@ -15,6 +15,35 @@ app.get("/api/posts", (req, res) => {
   res.status(200).send(posts);
 });
 
+// Get posts by id
+app.get("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const parseId = parseInt(id);
+
+  // If the Parsed Id is not a number
+  if (isNaN(parseId)) {
+    res.status(404).send({ msg: "Invalid Id" });
+  }
+
+  const findPostIndex = posts.findIndex((post) => post.id === parseId);
+
+  // If index not found
+  if (findPostIndex === -1) {
+    return res.status(404).send({ msg: "Post Not Found" });
+  }
+
+  req.findPostIndex = findPostIndex;
+
+  // If post not found
+  const post = posts[findPostIndex];
+  if (!post) {
+    return res.status(404).send({ msg: "Post Not Found" });
+  }
+
+  // Return the post
+  return res.status(200).send(post);
+});
+
 app.listen(PORT, (req, res) => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
